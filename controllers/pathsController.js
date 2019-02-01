1b389fb4e4636de85264d3edf17d00ac5c4d7077
Ryan Moore <rhcmoore@gmail.com>
2019-02-01 15:51:44 -0800

    Test db

--- controllers/pathsController.js
+++ controllers/pathsController.js
@@ -2,8 +2,10 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
+    console.log("Hitting findAll")
     db.Path.findAll({})
     .then(allData => {
+      console.log(allData, "AllData")
       res.json(allData) 
     })
       .catch(err => res.status(422).json(err));
