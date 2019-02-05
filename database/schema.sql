0106a6d07f59120809504a3ef0ea1d2b6d12b67a
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-05 10:40:27 -0800

    update schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,38 +1,3 @@
 DROP DATABASE IF EXISTS root_route_react;
 
 CREATE DATABASE root_route_react;
-
-USE root_route_react;
-
-CREATE TABLE Paths (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100),
-    description VARCHAR(500),
-    image VARCHAR(500),
-    createdAt VARCHAR(500),
-    updatedAt VARCHAR(500),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE Subjects (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100),
-    description VARCHAR(500),
-    image VARCHAR(500),
-    createdAt VARCHAR(500),
-    updatedAt VARCHAR(500),
-    PRIMARY KEY (id),
-    pathid INT
-);
-
-CREATE TABLE Resources (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100),
-    description VARCHAR(500),
-    image VARCHAR(500),
-    link VARCHAR(500),
-    createdAt VARCHAR(500),
-    updatedAt VARCHAR(500),
-    PRIMARY KEY (id),
-    subjectid INT
-);
