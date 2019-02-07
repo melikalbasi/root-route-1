7624d76d8749ba68a8e1f75d70b9172fd65fd9f4
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-07 15:53:20 -0800

    update client secret

---
+++ client/build/precache-manifest.b1521e7fe06614db937e91c2eea0a8f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff2bc9ec52525c0eef8e",
+    "url": "/static/css/main.5731ba5c.chunk.css"
+  },
+  {
+    "revision": "ff2bc9ec52525c0eef8e",
+    "url": "/static/js/main.ff2bc9ec.chunk.js"
+  },
+  {
+    "revision": "439ab7fcc6b751f0d7f1",
+    "url": "/static/js/1.439ab7fc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40d9f01fc3d7a66191433a3613143590",
+    "url": "/index.html"
+  }
+];
